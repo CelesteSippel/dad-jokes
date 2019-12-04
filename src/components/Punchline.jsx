d4d7435801d6f3242abeb71944b693fd16f3c270
Celeste Sippel <celestechristine@gmail.com>
2019-12-03 21:57:48 -0500

    punchline still not working

--- src/components/Punchline.jsx
+++ src/components/Punchline.jsx
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const Punchline = () => {
   const apiUrl = 'https://official-joke-api.appspot.com/random_joke'
-  const [jokes, setJokes] = useState([])
+  const [jokes, setJokes] = useState([[]])
+  // const [showText, setShowText] = useState(jokes.punchline)
   const getJokeData = async () => {
     const resp = await axios.get(apiUrl)
     setJokes(resp.data)
@@ -11,6 +12,7 @@ const Punchline = () => {
 
   useEffect(() => {
     getJokeData()
+    // setShowText()
   }, [])
 
   return (
@@ -20,7 +22,15 @@ const Punchline = () => {
           return (
             <section className="jokeBox" key={i}>
               <p>{joke.en}</p>
-              <p>{joke.punchline}</p>
+              <p>-{joke.punchline}</p>
+              {/* </section>
+            
+            <div className="buttons">
+            
+    
+            <button onClick={() => setShowText(jokes.punchline)}>Punchline</button>
+           
+          </div> */}
             </section>
           )
         })}
--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 const Setup = () => {
   const apiUrl = 'https://official-joke-api.appspot.com/random_joke'
   const [jokes, setJokes] = useState([])
-
+  // const [punchline, setPunchline] = useState([[]])
   const [showText, setShowText] = useState(false)
 
   const getJokesData = async () => {
@@ -26,20 +26,16 @@ const Setup = () => {
         <p>{jokes.en}</p>
         {showText && <p>-{jokes.setup}</p>}
       </section>
-      {/* <>
-        {jokes.map((joke, i) => {
-          return (
-            <section className="jokeBox" key={i}>
-              <p>{joke.en}</p>
-              <p>{joke.punchline}</p>
-            </section>
-          )
-        })}
-      </> */}
-      <section className="buttons">
-        <button onClick={() => setShowText(!showText)}>Get a Joke</button>
 
-        <button onClick={() => setShowText(!showText)}>Punchline</button>
+      {/* <section className="joke-box">
+        <p>{jokes.en}</p>
+        {showText && <p>-{jokes.punchline}</p>}
+      </section> */}
+
+      <section className="buttons">
+        <button onClick={() => setShowText(jokes.setup)}>Get a Joke</button>
+
+        <button onClick={() => setShowText(jokes.punchline)}>Punchline</button>
         <button onClick={() => refreshPage()}>Refresh</button>
       </section>
     </>
